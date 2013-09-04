e538ce6ee2a6f16f3ef877a67e82fa59a9519e79
ylma <ylma@weimi.me>
2013-09-05 02:01:58 +0800

    tower manager

--- WeiboDM/AppDelegate.h
+++ WeiboDM/AppDelegate.h
@@ -16,7 +16,9 @@
 <UIApplicationDelegate,
 OptionViewControllerDelegate,
 LoginViewControllerDelegate
-> 
+>
+
+///////////
 
 @property (strong, nonatomic) UIWindow *window;
 @property (strong, nonatomic) DMNavigationController *navVC;
